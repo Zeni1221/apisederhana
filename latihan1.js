bf4ec7cc57aac5af498e95892a478f13e4e55996
Zeni Nur Hasanah <zeninurhasannah@gmail.com>
2024-10-14 20:28:10 +0700

    file yang sudah di update

--- latihan1.js
+++ latihan1.js
@@ -17,16 +17,4 @@ fs.readFile('coba.json', 'utf-8', (err, data) => {
     console.log(`Status: ${Data.program.status}`);
     console.log(`Pembimbing 1: ${Data.program.pembimbing.pembimbing1}`);
     console.log(`Pembimbing 2: ${Data.program.pembimbing.pembimbing2}`);
-
-    // MAU LIHAT YANG PHP DULU 
-    // udah ilang ini file nya aku ganti 
-    // tdinya ini php tapi aku clear semua 
-
-    // ini jug akalo dijalanin bisa tapii aku masih ga paham aja 
-    // mangkanya mau liat yang kmu 
-
-    // coba jalanin deh ini baru bisa karena ada key program nya kan 
-
-
-
 });
